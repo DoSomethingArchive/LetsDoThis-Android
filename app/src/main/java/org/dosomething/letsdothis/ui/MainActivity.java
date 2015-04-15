ac94e8f22140e57f5e68a1446840172be8d84daa
apoorv <toidiu@gmail.com>
2015-04-15 12:30:32 -0400

    add ui folder

---
+++ app/src/main/java/org/dosomething/letsdothis/ui/MainActivity.java
@@ -0,0 +1,25 @@
+package org.dosomething.letsdothis.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+import org.dosomething.letsdothis.R;
+import org.dosomething.letsdothis.ui.fragments.BaseFragment;
+
+
+public class MainActivity extends ActionBarActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if(savedInstanceState == null)
+        {
+            getSupportFragmentManager().beginTransaction()
+                                       .add(R.id.container, BaseFragment.newInstance()).commit();
+        }
+    }
+
+}
