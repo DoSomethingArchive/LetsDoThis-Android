0511a09efcaa29f547a7b2d909a020e782aad5eb
apoorv <toidiu@gmail.com>
2015-04-16 16:37:08 -0400

    add network error handler task. add user profile activity

---
+++ app/src/main/java/org/dosomething/letsdothis/network/models/UserListResponse.java
@@ -0,0 +1,33 @@
+package org.dosomething.letsdothis.network.models;
+import org.dosomething.letsdothis.data.User;
+
+/**
+ * Created by toidiu on 4/16/15.
+ */
+public class UserListResponse
+{
+
+   public Integer total;
+   public Integer current_page;
+   public Integer last_page;
+   public User[]  data;
+
+    //    {
+    //        "total": 23,
+    //        "per_page": 5,
+    //        "current_page": 1,
+    //        "last_page": 5,
+    //        "from": 1,
+    //        "to": 5,
+    //        "data": [
+    //            {
+    //                "_id": "5480c950bffebc651c8b456f",
+    //                    "email": "test@dosomething.org",
+    //                ... the rest of this user data...
+    //            },
+    //            {
+    //                ...
+    //            }
+    //        ]
+    //    }
+}
