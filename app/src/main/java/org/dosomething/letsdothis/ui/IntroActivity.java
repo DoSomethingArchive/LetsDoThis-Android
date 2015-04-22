500046a75ea52b84fe5c6780b86da1e08fad53e9
apoorv <toidiu@gmail.com>
2015-04-22 18:07:51 -0400

    change intro to view pager

--- app/src/main/java/org/dosomething/letsdothis/ui/IntroActivity.java
+++ app/src/main/java/org/dosomething/letsdothis/ui/IntroActivity.java
@@ -3,18 +3,29 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
 
+import com.viewpagerindicator.TabPageIndicator;
+
 import org.dosomething.letsdothis.R;
+import org.dosomething.letsdothis.ui.fragments.BaseIntroFragment;
 import org.dosomething.letsdothis.ui.fragments.Intro1Fragment;
-import org.dosomething.letsdothis.utils.AppPrefs;
+import org.dosomething.letsdothis.ui.fragments.Intro2Fragment;
+import org.dosomething.letsdothis.ui.fragments.Intro3Fragment;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by toidiu on 4/15/15.
  */
-public class IntroActivity extends ActionBarActivity
+public class IntroActivity extends ActionBarActivity implements BaseIntroFragment.PagerChangeListener
 {
     private static final String TAG = IntroActivity.class.getSimpleName();
+    private ViewPager      pager;
+    private List<Fragment> fragmentList;
 
     //~=~=~=~=~=~=~=~=~=~=~=~=Views
 
@@ -29,33 +40,64 @@ public class IntroActivity extends ActionBarActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
 
-        if(AppPrefs.getInstance(this).isLoggedIn())
+        initPager();
+    }
+
+    private void initPager()
+    {
+        fragmentList = new ArrayList<>();
+        fragmentList.add(Intro1Fragment.newInstance());
+        fragmentList.add(Intro2Fragment.newInstance());
+        fragmentList.add(Intro3Fragment.newInstance());
+
+        pager = (ViewPager) findViewById(R.id.pager);
+        pager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager())
         {
-            goToMain();
+            @Override
+            public int getCount()
+            {
+                return fragmentList.size();
+            }
+
+            @Override
+            public Fragment getItem(int position)
+            {
+                return fragmentList.get(position);
+            }
+
+            @Override
+            public CharSequence getPageTitle(int position)
+            {
+                return Integer.toString(position);
+            }
+        });
+
+        TabPageIndicator indicator = (TabPageIndicator) findViewById(R.id.indicator);
+        indicator.setViewPager(pager);
+    }
+
+    @Override
+    public void navigatePrev()
+    {
+        int currentItem = pager.getCurrentItem();
+        if(currentItem != 0)
+        {
+            pager.setCurrentItem(currentItem - 1);
+        }
+    }
+
+    @Override
+    public void navigateNext()
+    {
+        int currentItem = pager.getCurrentItem();
+        if(currentItem == (fragmentList.size() - 1))
+        {
+            startActivity(RegisterLoginActivity.getLaunchIntent(this));
+            finish();
         }
         else
         {
-            initIntroFragment();
+            pager.setCurrentItem(currentItem + 1);
         }
     }
-
-    private void initIntroFragment()
-    {
-        getSupportFragmentManager().beginTransaction()
-                .add(R.id.container, Intro1Fragment.newInstance(), null).commit();
-
-    }
-
-    private void goToMain()
-    {
-        startActivity(MainActivity.getLaunchIntent(this));
-        finish();
-    }
-
-    public void replaceCurrentFragment(Fragment fragment, String tag)
-    {
-        getSupportFragmentManager().beginTransaction().addToBackStack(tag)
-                .replace(R.id.container, fragment, tag).commit();
-    }
-
 }
--- app/src/main/java/org/dosomething/letsdothis/ui/fragments/BaseIntroFragment.java
+++ app/src/main/java/org/dosomething/letsdothis/ui/fragments/BaseIntroFragment.java
@@ -1,23 +1,33 @@
 package org.dosomething.letsdothis.ui.fragments;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.View;
 
 import org.dosomething.letsdothis.R;
-import org.dosomething.letsdothis.ui.IntroActivity;
 
 /**
  * Created by toidiu on 4/21/15.
  */
 public class BaseIntroFragment extends Fragment
 {
-    protected void initIntroNavigation(View view, final Fragment next)
+
+    private PagerChangeListener listener;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        listener = (PagerChangeListener) getActivity();
+    }
+
+    protected void initIntroNavigation(View view)
     {
         view.findViewById(R.id.prev).setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View view)
             {
-                getActivity().getSupportFragmentManager().popBackStackImmediate();
+                listener.navigatePrev();
             }
         });
         view.findViewById(R.id.next).setOnClickListener(new View.OnClickListener()
@@ -25,11 +35,15 @@ public class BaseIntroFragment extends Fragment
             @Override
             public void onClick(View view)
             {
-                if(next != null)
-                {
-                    ((IntroActivity) getActivity()).replaceCurrentFragment(next, null);
-                }
+                    listener.navigateNext();
             }
         });
+    }
+
+    public interface PagerChangeListener
+    {
+        void navigatePrev();
+
+        void navigateNext();
     }
 }
