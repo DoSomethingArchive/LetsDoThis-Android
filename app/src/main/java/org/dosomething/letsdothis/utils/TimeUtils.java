d9809154f7b89eb67fdabe1f7000b84528395880
apoorv <toidiu@gmail.com>
2015-04-29 18:00:01 -0400

    add grayscale image

--- app/src/main/java/org/dosomething/letsdothis/utils/TimeUtils.java
+++ app/src/main/java/org/dosomething/letsdothis/utils/TimeUtils.java
@@ -2,11 +2,18 @@ package org.dosomething.letsdothis.utils;
 import android.content.Context;
 import android.text.format.DateUtils;
 
+import org.dosomething.letsdothis.LDTApplication;
 import org.dosomething.letsdothis.R;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+
+
+import static org.dosomething.letsdothis.ui.views.typeface.CustomTypefaceSpan.format;
+import static org.dosomething.letsdothis.ui.views.typeface.TypefaceManager.BRANDON_BOLD;
 
 /**
  * Created by izzyoji :) on 4/27/15.
@@ -27,17 +34,17 @@ public class TimeUtils
             count = (int) (duration / DateUtils.SECOND_IN_MILLIS);
             return context.getResources().getQuantityString(R.plurals.seconds_ago, count, count);
         }
-        else if (duration < DateUtils.HOUR_IN_MILLIS)
+        else if(duration < DateUtils.HOUR_IN_MILLIS)
         {
             count = (int) (duration / DateUtils.MINUTE_IN_MILLIS);
             return context.getResources().getQuantityString(R.plurals.minutes_ago, count, count);
         }
-        else if (duration < DateUtils.DAY_IN_MILLIS)
+        else if(duration < DateUtils.DAY_IN_MILLIS)
         {
             count = (int) (duration / DateUtils.HOUR_IN_MILLIS);
             return context.getResources().getQuantityString(R.plurals.hours_ago, count, count);
         }
-        else if (duration < DateUtils.WEEK_IN_MILLIS)
+        else if(duration < DateUtils.WEEK_IN_MILLIS)
         {
             count = (int) (duration / DateUtils.DAY_IN_MILLIS);
             return context.getResources().getQuantityString(R.plurals.days_ago, count, count);
@@ -56,26 +63,31 @@ public class TimeUtils
         return formatter.format(new Date(millis));
     }
 
-    public static String getTimeUntilExpiration(Long expTimeInMillis)
+    public static List<String> getTimeUntilExpiration(Long expireTimeInMillis)
     {
+        List<String> expire = new ArrayList<>();
+
         long now = System.currentTimeMillis();
-        long timeRemaining = expTimeInMillis - now;
+        long timeRemaining = expireTimeInMillis + DateUtils.HOUR_IN_MILLIS + DateUtils.MINUTE_IN_MILLIS * 3 - now;
+        String days = "0";
+        String hours = "0";
+        String mins = "0";
 
-        String HH = Long.toString(timeRemaining / DateUtils.HOUR_IN_MILLIS);
-        timeRemaining %= DateUtils.HOUR_IN_MILLIS;
-        String MM = Long.toString(timeRemaining / DateUtils.MINUTE_IN_MILLIS);
-        timeRemaining %= DateUtils.MINUTE_IN_MILLIS;
-        if (MM.length() < 2)
+        if(timeRemaining > 0)
         {
-            MM = "0" + MM;
-        }
-        String ss = Long.toString(timeRemaining / 1000);
-        if (ss.length() < 2)
-        {
-            ss = "0" + ss;
+            days = Long.toString(timeRemaining / DateUtils.DAY_IN_MILLIS);
+            timeRemaining %= DateUtils.DAY_IN_MILLIS;
+            hours = Long.toString(timeRemaining / DateUtils.HOUR_IN_MILLIS);
+            timeRemaining %= DateUtils.HOUR_IN_MILLIS;
+            mins = Long.toString(timeRemaining / DateUtils.MINUTE_IN_MILLIS);
         }
 
-        return HH + ":" + MM + ":" + ss;
+        Context context = LDTApplication.getContext();
+        expire.add(format(context, days, BRANDON_BOLD).toString());
+        expire.add(format(context, hours, BRANDON_BOLD).toString());
+        expire.add(format(context, mins, BRANDON_BOLD).toString());
+
+        return expire;
     }
 
 }
