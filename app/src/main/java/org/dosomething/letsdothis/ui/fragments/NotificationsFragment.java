439bab7adcd4935957495a28a2d8dc402f313b63
apoorv <toidiu@gmail.com>
2015-05-01 11:17:25 -0400

    added list view and adapter for notification activity

--- app/src/main/java/org/dosomething/letsdothis/ui/fragments/NotificationsFragment.java
+++ app/src/main/java/org/dosomething/letsdothis/ui/fragments/NotificationsFragment.java
@@ -1,11 +1,15 @@
 package org.dosomething.letsdothis.ui.fragments;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import org.dosomething.letsdothis.R;
+import org.dosomething.letsdothis.ui.adapters.NotificationListAdapter;
 
 /**
  * Created by izzyoji :) on 4/15/15.
@@ -20,8 +24,24 @@ public class NotificationsFragment extends Fragment
     }
 
     @Override
+    public void onCreate(@Nullable Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
-        return inflater.inflate(R.layout.fragment_notifications, container, false);
+         inflater.inflate(R.layout.fragment_notifications, container, false);
+        ArrayAdapter adapter = new NotificationListAdapter(getActivity(), 0);
+        ListView listView = (ListView) container.findViewById(R.id.list);
+        listView.setAdapter(adapter);
+        adapter.add("");
+        adapter.add("");
+        adapter.add("");
+        adapter.add("");
+        adapter.notifyDataSetChanged();
     }
+
+
 }
