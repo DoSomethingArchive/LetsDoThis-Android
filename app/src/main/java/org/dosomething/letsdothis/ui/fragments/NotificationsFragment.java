5b43b100f4d0141c3a4e9d018f71bfe87f142406
eoji <eoji11592@gmail.com>
2015-05-01 13:15:59 -0400

    Added sample data to notifications screen. Created Notification object and adapter.

--- app/src/main/java/org/dosomething/letsdothis/ui/fragments/NotificationsFragment.java
+++ app/src/main/java/org/dosomething/letsdothis/ui/fragments/NotificationsFragment.java
@@ -2,14 +2,18 @@ package org.dosomething.letsdothis.ui.fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
 import org.dosomething.letsdothis.R;
-import org.dosomething.letsdothis.ui.adapters.NotificationListAdapter;
+import org.dosomething.letsdothis.data.Notification;
+import org.dosomething.letsdothis.ui.adapters.NotificationAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by izzyoji :) on 4/15/15.
@@ -24,24 +28,39 @@ public class NotificationsFragment extends Fragment
     }
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState)
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
-        super.onCreate(savedInstanceState);
+        return inflater.inflate(R.layout.fragment_notifications, container, false);
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    public void onActivityCreated(@Nullable Bundle savedInstanceState)
     {
-         inflater.inflate(R.layout.fragment_notifications, container, false);
-        ArrayAdapter adapter = new NotificationListAdapter(getActivity(), 0);
-        ListView listView = (ListView) container.findViewById(R.id.list);
-        listView.setAdapter(adapter);
-        adapter.add("");
-        adapter.add("");
-        adapter.add("");
-        adapter.add("");
-        adapter.notifyDataSetChanged();
+        super.onActivityCreated(savedInstanceState);
+        //FIXME: get real data
+        NotificationAdapter adapter = new NotificationAdapter(generateSampleData());
+        RecyclerView recyclerView = (RecyclerView) getView().findViewById(R.id.recycler);
+        recyclerView.setAdapter(adapter);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+
+        recyclerView.setLayoutManager(layoutManager);
+
     }
 
+    private List<Notification> generateSampleData()
+    {
+        List<Notification> notifications = new ArrayList<>();
+        for(int i = 0; i < 10; i++)
+        {
+            Notification notification = new Notification();
+            notification.title = "Sample notification " + (i + 1);
+            notification.details = "Here are the fabulous details. ";
+            notification.imagePath = "http://icons.iconarchive.com/icons/carlosjj/google-jfk/128/android-icon.png";
+            notification.timeStamp = System.currentTimeMillis() - (i * 1000);
+            notifications.add(notification);
+        }
 
+        return notifications;
+    }
 }
