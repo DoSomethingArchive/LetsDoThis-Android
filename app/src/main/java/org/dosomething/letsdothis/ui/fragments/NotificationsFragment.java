d2988aea9c812db719285c3ea428249935dd92fa
apoorv <toidiu@gmail.com>
2015-05-04 17:03:58 -0400

    make notification extend abstract quick return fragment

--- app/src/main/java/org/dosomething/letsdothis/ui/fragments/NotificationsFragment.java
+++ app/src/main/java/org/dosomething/letsdothis/ui/fragments/NotificationsFragment.java
@@ -1,10 +1,8 @@
 package org.dosomething.letsdothis.ui.fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,15 +18,10 @@ import java.util.List;
 /**
  * Created by izzyoji :) on 4/15/15.
  */
-public class NotificationsFragment extends Fragment
+public class NotificationsFragment extends AbstractQuickReturnFragment
 {
 
     public static final String TAG = NotificationsFragment.class.getSimpleName();
-    //~=~=~=~=~=~=~=~=~=~=~=~=QuickReturn
-    private Toolbar             toolbar;
-    private RecyclerView        recycleView;
-    private LinearLayoutManager layoutManager;
-
 
     public static NotificationsFragment newInstance()
     {
@@ -42,91 +35,9 @@ public class NotificationsFragment extends Fragment
 
         recycleView = (RecyclerView) rootView.findViewById(R.id.recycler);
         toolbar = ((MainActivity) getActivity()).toolbar;
-        recycleView.setOnScrollListener(getScrollListener());
+
         return rootView;
     }
-
-
-    private RecyclerView.OnScrollListener getScrollListener()
-    {
-        RecyclerView.OnScrollListener onScrollListener = new RecyclerView.OnScrollListener()
-        {
-            private int toolbarHeight;
-
-            @Override
-            public void onScrollStateChanged(RecyclerView recyclerView, int newState)
-            {
-                super.onScrollStateChanged(recyclerView, newState);
-                if(layoutManager.findFirstVisibleItemPosition() == 0)
-                {
-                    return;
-                }
-                if(newState != RecyclerView.SCROLL_STATE_IDLE)
-                {
-                    return;
-                }
-
-                float translate = toolbar.getTranslationY();
-                if(toolbarHeight <= - translate)
-                {
-                    //hide
-                    toolbar.setTranslationY(- toolbarHeight);
-                }
-                else if(translate >= 0)
-                {
-                    //show
-                    toolbar.setTranslationY(0);
-                }
-
-                int half = toolbarHeight / 2;
-                if(half > - translate)
-                {
-                    //show
-                    toolbar.animate().setDuration(200).translationY(0);
-                }
-                else
-                {
-                    //hide
-                    toolbar.animate().setDuration(200).translationY(- toolbarHeight);
-                }
-
-
-            }
-
-            @Override
-            public void onScrolled(RecyclerView recyclerView, int dx, int dy)
-            {
-                super.onScrolled(recyclerView, dx, dy);
-
-                float toolbarTranslate = toolbar.getTranslationY();
-                toolbarHeight = toolbar.getHeight();
-                boolean hideScroll = dy > 0;
-
-                if(hideScroll)
-                {
-                    if(toolbarHeight <= - toolbarTranslate)
-                    {
-                        //hide
-                        toolbar.setTranslationY(- toolbarHeight);
-                        return;
-                    }
-                    toolbar.setTranslationY(toolbarTranslate - dy);
-                }
-                else
-                {
-                    if(0 <= toolbarTranslate)
-                    {
-                        //show
-                        toolbar.setTranslationY(0);
-                        return;
-                    }
-                    toolbar.setTranslationY(toolbarTranslate - dy);
-                }
-            }
-        };
-        return onScrollListener;
-    }
-
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState)
