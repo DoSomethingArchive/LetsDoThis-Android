76a2b3330b351952fb35c17f71326b30a2560966
apoorv <toidiu@gmail.com>
2015-05-08 14:49:19 -0400

    style the report back upload activity

--- app/src/main/java/org/dosomething/letsdothis/ui/ReportBackUploadActivity.java
+++ app/src/main/java/org/dosomething/letsdothis/ui/ReportBackUploadActivity.java
@@ -3,12 +3,15 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
 
 import org.dosomething.letsdothis.R;
+import org.dosomething.letsdothis.ui.views.SlantedBackgroundDrawable;
 
 import java.io.File;
 
@@ -17,8 +20,6 @@ import java.io.File;
  */
 public class ReportBackUploadActivity extends AppCompatActivity
 {
-
-
     public static final String CROPPED_SQUARE = "CROPPED_SQUARE";
 
     public static Intent getLaunchIntent(Context context, String cropedSquare)
@@ -41,7 +42,24 @@ public class ReportBackUploadActivity extends AppCompatActivity
         Picasso.with(this).load(new File(croppedImage)).into(image);
         Picasso.with(this).load(new File(croppedImage)).into(imageHero);
 
+        View wrapper = findViewById(R.id.image_wrapper);
+        //        wrapper.setBackground(new ShadowBackgroundDrawable());
+        wrapper.setBackground(new SlantedBackgroundDrawable(true));
+        wrapper.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+
         EditText caption = (EditText) findViewById(R.id.caption);
         EditText number = (EditText) findViewById(R.id.number);
+
+
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+        findViewById(R.id.upload).setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                //FIXME do the upload
+                finish();
+            }
+        });
     }
 }
