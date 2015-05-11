9d601f2e2a532b310d1506919f122911bf03943b
eoji <eoji11592@gmail.com>
2015-05-11 17:47:05 -0400

    Back buttons in PhotoCropActivity and ReportBackUploadActivity finish activity. Added report back submission flow to GroupActivity and HubFragment.

--- app/src/main/java/org/dosomething/letsdothis/ui/fragments/HubFragment.java
+++ app/src/main/java/org/dosomething/letsdothis/ui/fragments/HubFragment.java
@@ -1,10 +1,15 @@
 package org.dosomething.letsdothis.ui.fragments;
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -13,17 +18,22 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import org.dosomething.letsdothis.BuildConfig;
 import org.dosomething.letsdothis.R;
+import org.dosomething.letsdothis.data.Campaign;
 import org.dosomething.letsdothis.data.User;
 import org.dosomething.letsdothis.tasks.GetCurrentUserCampaignTask;
 import org.dosomething.letsdothis.tasks.GetPastUserCampaignTask;
 import org.dosomething.letsdothis.ui.GroupActivity;
+import org.dosomething.letsdothis.ui.PhotoCropActivity;
 import org.dosomething.letsdothis.ui.SettingsActivity;
 import org.dosomething.letsdothis.ui.UserListActivity;
 import org.dosomething.letsdothis.ui.UserProfileActivity;
 import org.dosomething.letsdothis.ui.UserUpdateActivity;
 import org.dosomething.letsdothis.ui.adapters.HubAdapter;
 import org.dosomething.letsdothis.utils.AppPrefs;
+
+import java.io.File;
 
 import co.touchlab.android.threading.eventbus.EventBusExt;
 import co.touchlab.android.threading.tasks.TaskQueue;
@@ -35,10 +45,12 @@ public class HubFragment extends AbstractQuickReturnFragment implements HubAdapt
 {
 
     //~=~=~=~=~=~=~=~=~=~=~=~=Constants
-    public static final String TAG = HubFragment.class.getSimpleName();
+    public static final  String TAG            = HubFragment.class.getSimpleName();
+    private static final int    SELECT_PICTURE = 55;
 
     private HubAdapter         adapter;
     private SetToolbarListener setToolbarListener;
+    private Uri                imageUri;
 
     public static HubFragment newInstance()
     {
@@ -65,9 +77,9 @@ public class HubFragment extends AbstractQuickReturnFragment implements HubAdapt
         EventBusExt.getDefault().register(this);
         String currentUserId = AppPrefs.getInstance(getActivity()).getCurrentUserId();
         TaskQueue.loadQueueDefault(getActivity())
-                .execute(new GetCurrentUserCampaignTask(currentUserId));
+                 .execute(new GetCurrentUserCampaignTask(currentUserId));
         TaskQueue.loadQueueDefault(getActivity())
-                .execute(new GetPastUserCampaignTask(currentUserId));
+                 .execute(new GetPastUserCampaignTask(currentUserId));
 
         View rootView = inflater
                 .inflate(R.layout.activity_fragment_quickreturn_recycler, container, false);
@@ -86,6 +98,7 @@ public class HubFragment extends AbstractQuickReturnFragment implements HubAdapt
     public void onStop()
     {
         super.onStop();
+        //FIXME I don't think this is the right place for this
         EventBusExt.getDefault().unregister(this);
     }
 
@@ -151,7 +164,79 @@ public class HubFragment extends AbstractQuickReturnFragment implements HubAdapt
     @Override
     public void groupClicked(int campaignId, String userId)
     {
-        startActivity(GroupActivity.getLaunchIntent(getActivity(), campaignId, userId ));
+        startActivity(GroupActivity.getLaunchIntent(getActivity(), campaignId, userId));
+    }
+
+    @Override
+    public void onProveShareClicked(Campaign campaign)
+    {
+        choosePicture();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if(resultCode == Activity.RESULT_OK)
+        {
+            if(requestCode == SELECT_PICTURE)
+            {
+                final boolean isCamera;
+                if(data == null)
+                {
+                    isCamera = true;
+                }
+                else
+                {
+                    final String action = data.getAction();
+                    if(action == null)
+                    {
+                        isCamera = false;
+                    }
+                    else
+                    {
+                        isCamera = action.equals(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                    }
+                }
+
+                Uri selectedImageUri;
+                if(isCamera)
+                {
+                    selectedImageUri = imageUri;
+                }
+                else
+                {
+                    selectedImageUri = data.getData();
+                }
+
+                startActivity(PhotoCropActivity
+                                      .getLaunchIntent(getActivity(), selectedImageUri.toString()));
+            }
+        }
+    }
+
+
+    public void choosePicture()
+    {
+        Intent pickIntent = new Intent(Intent.ACTION_PICK,
+                                       android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        pickIntent.setType("image/*");
+
+        Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        File externalFile = new File(getActivity().getExternalFilesDir(
+                Environment.DIRECTORY_PICTURES),
+                                     "report_back" + System.currentTimeMillis() + ".jpg");
+        imageUri = Uri.fromFile(externalFile);
+        if(BuildConfig.DEBUG)
+        {
+            Log.d("photo location", imageUri.toString());
+        }
+        takePhotoIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+
+        String pickTitle = getString(R.string.select_picture);
+        Intent chooserIntent = Intent.createChooser(takePhotoIntent, pickTitle);
+        chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[] {pickIntent});
+
+        startActivityForResult(chooserIntent, SELECT_PICTURE);
     }
 
 
