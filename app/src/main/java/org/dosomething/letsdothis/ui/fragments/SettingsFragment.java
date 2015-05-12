74f27b2b0739517b81ddf0c27c0a372151d48e3a
eoji <eoji11592@gmail.com>
2015-05-12 17:56:59 -0400

    Added prompt dialog for logout. Dialog needs styling.

--- app/src/main/java/org/dosomething/letsdothis/ui/fragments/SettingsFragment.java
+++ app/src/main/java/org/dosomething/letsdothis/ui/fragments/SettingsFragment.java
@@ -1,18 +1,14 @@
 package org.dosomething.letsdothis.ui.fragments;
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.text.TextUtils;
 
 import org.dosomething.letsdothis.R;
-import org.dosomething.letsdothis.tasks.LogoutTask;
 import org.dosomething.letsdothis.ui.BaseActivity;
-import org.dosomething.letsdothis.ui.RegisterLoginActivity;
-
-import co.touchlab.android.threading.tasks.TaskQueue;
 
 /**
  * Created by izzyoji :) on 4/29/15.
@@ -40,21 +36,16 @@ public class SettingsFragment extends PreferenceFragment implements SharedPrefer
                     }
                 });
 
-        findPreference(getString(R.string.logout)).setOnPreferenceClickListener(new Preference.OnPreferenceClickListener()
-        {
-            @Override
-            public boolean onPreferenceClick(Preference preference)
-            {
-//                BaseActivity.logOutUser(getActivity());
-                return true;
-            }
-        });
 
     }
 
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key)
     {
+        if(TextUtils.equals(getString(R.string.logout), key))
+        {
+            BaseActivity.logOutUser(getActivity());
+        }
     }
 
 }
