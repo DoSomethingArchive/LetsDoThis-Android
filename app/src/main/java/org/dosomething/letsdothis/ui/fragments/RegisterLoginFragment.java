fd5b32a1ac660eff8279fa1ab6cec2c9585b2572
apoorv <toidiu@gmail.com>
2015-06-30 17:19:48 -0400

    request focus to teh edit text

--- app/src/main/java/org/dosomething/letsdothis/ui/fragments/RegisterLoginFragment.java
+++ app/src/main/java/org/dosomething/letsdothis/ui/fragments/RegisterLoginFragment.java
@@ -60,7 +60,7 @@ public class RegisterLoginFragment extends Fragment
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
-        View rootView = inflater.inflate(R.layout.fragment_register, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_register_login, container, false);
         initAppNavigation(rootView);
         initFbConnect(rootView);
 
