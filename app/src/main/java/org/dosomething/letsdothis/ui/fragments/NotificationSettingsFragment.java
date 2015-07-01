cee33526b3a4537dd4ba71b9e5178455e99c271f
apoorv <toidiu@gmail.com>
2015-07-01 11:12:36 -0400

    style the settings page

--- app/src/main/java/org/dosomething/letsdothis/ui/fragments/NotificationSettingsFragment.java
+++ app/src/main/java/org/dosomething/letsdothis/ui/fragments/NotificationSettingsFragment.java
@@ -1,4 +1,6 @@
 package org.dosomething.letsdothis.ui.fragments;
+import android.app.Activity;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
@@ -11,6 +13,9 @@ import org.dosomething.letsdothis.R;
  */
 public class NotificationSettingsFragment extends PreferenceFragment
 {
+
+    private NotificationsFragment.SetTitleListener setTitleListener;
+
     public static NotificationSettingsFragment newInstance()
     {
         return new NotificationSettingsFragment();
@@ -24,19 +29,58 @@ public class NotificationSettingsFragment extends PreferenceFragment
         initChangeNotifications();
     }
 
+    @Override
+    public void onAttach(Activity activity)
+    {
+        super.onAttach(activity);
+        setTitleListener = (NotificationsFragment.SetTitleListener) getActivity();
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        setTitleListener.setTitle(getResources().getString(R.string.notification_pref));
+    }
+
     private void initChangeNotifications()
     {
-        findPreference("setting_1")
+        Resources res = getResources();
+
+        findPreference(res.getString(R.string.notification_kudos))
                 .setOnPreferenceClickListener(new Preference.OnPreferenceClickListener()
                 {
                     @Override
                     public boolean onPreferenceClick(Preference preference)
                     {
                         //FIXME-----
-                        Log.d("------", "----------NOTIFY");
+                        Log.d("------", "----------ku");
+                        return true;
+                    }
+                });
+        findPreference(res.getString(R.string.notification_campaign))
+                .setOnPreferenceClickListener(new Preference.OnPreferenceClickListener()
+                {
+                    @Override
+                    public boolean onPreferenceClick(Preference preference)
+                    {
+                        //FIXME-----
+                        Log.d("------", "----------cam");
+                        return true;
+                    }
+                });
+        findPreference(res.getString(R.string.notification_friend))
+                .setOnPreferenceClickListener(new Preference.OnPreferenceClickListener()
+                {
+                    @Override
+                    public boolean onPreferenceClick(Preference preference)
+                    {
+                        //FIXME-----
+                        Log.d("------", "----------fri");
                         return true;
                     }
                 });
     }
+
 
 }
