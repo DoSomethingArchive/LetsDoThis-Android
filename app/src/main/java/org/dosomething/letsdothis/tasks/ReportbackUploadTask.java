f3e35a63b61cf93b2e0f207391619a577bbcdc36
apoorv <toidiu@gmail.com>
2015-07-07 14:21:57 -0400

    submit actual file for report back

--- app/src/main/java/org/dosomething/letsdothis/tasks/ReportbackUploadTask.java
+++ app/src/main/java/org/dosomething/letsdothis/tasks/ReportbackUploadTask.java
@@ -1,16 +1,17 @@
 package org.dosomething.letsdothis.tasks;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Base64;
 
+import org.apache.commons.io.output.ByteArrayOutputStream;
 import org.dosomething.letsdothis.network.NetworkHelper;
 import org.dosomething.letsdothis.network.models.RequestReportback;
-import org.dosomething.letsdothis.network.models.ResponseReportbackSubmit;
 import org.dosomething.letsdothis.network.models.ResponseSubmitReportBack;
 import org.dosomething.letsdothis.utils.AppPrefs;
 
-import java.util.Date;
-
 import co.touchlab.android.threading.eventbus.EventBusExt;
-import retrofit.client.Response;
+import co.touchlab.android.threading.tasks.TaskQueue;
 
 /**
  * Created by izzyoji :) on 5/15/15.
@@ -19,22 +20,40 @@ public class ReportbackUploadTask extends BaseNetworkErrorHandlerTask
 {
 
     private final RequestReportback req;
+    private final String            filePath;
     private final int               campaignId;
 
-    public ReportbackUploadTask(RequestReportback req, int campaignId)
+
+    public static void uploadReport(Context context, RequestReportback req, int campaignId, String filePath)
+    {
+        TaskQueue.loadQueue(context, "reportBack")
+                .execute(new ReportbackUploadTask(req, campaignId, filePath));
+    }
+
+    private ReportbackUploadTask(RequestReportback req, int campaignId, String filePath)
     {
         this.req = req;
         this.campaignId = campaignId;
+        this.filePath = filePath;
     }
 
     @Override
     protected void run(Context context) throws Throwable
     {
-        req.file = "test";
+        req.file = base64Encode(filePath);
         req.why_participated = "Doing something!";
         String sessionToken = AppPrefs.getInstance(context).getSessionToken();
         ResponseSubmitReportBack response = NetworkHelper.getNorthstarAPIService()
                 .submitReportback(sessionToken, req, campaignId);
+    }
+
+    private String base64Encode(String filePath)
+    {
+        Bitmap bm = BitmapFactory.decodeFile(filePath);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bm.compress(Bitmap.CompressFormat.JPEG, 100, baos); //bm is the bitmap object
+        byte[] b = baos.toByteArray();
+        return Base64.encodeToString(b, Base64.DEFAULT);
     }
 
     @Override
@@ -43,4 +62,5 @@ public class ReportbackUploadTask extends BaseNetworkErrorHandlerTask
         super.onComplete(context);
         EventBusExt.getDefault().post(this);
     }
+
 }
