a15c86b2e216656bcca34f4f9268d8d8225c2c2b
Jon Uy <jonxuy@gmail.com>
2015-08-19 19:03:06 -0400

    Adding Crashlytics support for Fabric distribution - For info on fabric.properties setup needed to build and distribute on Fabric, see https://github.com/DoSomething/ServerConfig/wiki/3.0-Mobile-app:-Let's-Do-This

--- app/src/main/java/org/dosomething/letsdothis/LDTApplication.java
+++ app/src/main/java/org/dosomething/letsdothis/LDTApplication.java
@@ -2,8 +2,10 @@ package org.dosomething.letsdothis;
 import android.app.Application;
 import android.content.Context;
 
+import com.crashlytics.android.Crashlytics;
 import com.facebook.FacebookSdk;
 import com.parse.Parse;
+import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by izzyoji :) on 4/14/15.
@@ -18,6 +20,7 @@ public class LDTApplication extends Application
         super.onCreate();
         context = this;
 
+        Fabric.with(this, new Crashlytics());
         FacebookSdk.sdkInitialize(getApplicationContext());
         Parse.initialize(this, getString(R.string.parse_app_id), getString(R.string.parse_client_key));
     }
