08f67369e7d68582752f3db75bc3f543692ec9cb
Jon Uy <jonxuy@gmail.com>
2015-09-14 17:59:17 -0400

    Validate input fields before registering - shows Toast error message on error and highlights input field in red

--- app/src/main/java/org/dosomething/letsdothis/ui/RegisterActivity.java
+++ app/src/main/java/org/dosomething/letsdothis/ui/RegisterActivity.java
@@ -5,11 +5,11 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.design.widget.Snackbar;
-import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.facebook.login.LoginManager;
 import com.squareup.picasso.Picasso;
@@ -19,9 +19,7 @@ import org.dosomething.letsdothis.R;
 import org.dosomething.letsdothis.data.FbUser;
 import org.dosomething.letsdothis.tasks.RegisterTask;
 import org.dosomething.letsdothis.tasks.UploadAvatarTask;
-import org.dosomething.letsdothis.ui.adapters.InvitesAdapter;
 import org.dosomething.letsdothis.utils.AppPrefs;
-import org.dosomething.letsdothis.utils.Hashery;
 import org.dosomething.letsdothis.utils.ViewUtils;
 
 import java.io.File;
@@ -41,6 +39,7 @@ public class RegisterActivity extends BaseActivity
 
     //~=~=~=~=~=~=~=~=~=~=~=~=Views
     private EditText  email;
+    private EditText  phone;
     private EditText  password;
     private EditText  firstName;
     private ImageView avatar;
@@ -64,6 +63,11 @@ public class RegisterActivity extends BaseActivity
         initLightning();
 
         FbUser fbUser = (FbUser) getIntent().getSerializableExtra(FB_USER);
+
+        email = (EditText) findViewById(R.id.email);
+        phone = (EditText) findViewById(R.id.phone);
+        password = (EditText) findViewById(R.id.password);
+        firstName = (EditText) findViewById(R.id.first_name);
 
         avatar = (ImageView) findViewById(R.id.avatar);
         avatar.setOnClickListener(new View.OnClickListener()
@@ -106,9 +110,42 @@ public class RegisterActivity extends BaseActivity
 
         String pickTitle = getString(R.string.select_picture);
         Intent chooserIntent = Intent.createChooser(takePhotoIntent, pickTitle);
-        chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[] {pickIntent});
+        chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[]{pickIntent});
 
         startActivityForResult(chooserIntent, SELECT_PICTURE);
+    }
+
+    /**
+     * Ensures fields are valid to be sent for a registration request.
+     */
+    private boolean validateForRegistration() {
+        boolean isValid = true;
+        firstName.setBackgroundResource(R.drawable.bg_white_rounded_rect_filled);
+        email.setBackgroundResource(R.drawable.bg_white_rounded_rect_filled);
+        password.setBackgroundResource(R.drawable.bg_white_rounded_rect_filled);
+
+        String inputFName = firstName.getText().toString();
+        if (inputFName.isEmpty()) {
+            firstName.setBackgroundResource(R.drawable.edittext_error_background);
+            Toast.makeText(RegisterActivity.this, R.string.error_registration_first_name, Toast.LENGTH_SHORT).show();
+            isValid = false;
+        }
+
+        String inputEmail = email.getText().toString();
+        if (inputEmail.isEmpty() || !inputEmail.matches(".+[@].+[.].+")) {
+            email.setBackgroundResource(R.drawable.edittext_error_background);
+            Toast.makeText(RegisterActivity.this, R.string.error_registration_email, Toast.LENGTH_SHORT).show();
+            isValid = false;
+        }
+
+        String inputPassword = password.getText().toString();
+        if (inputPassword.length() < 6) {
+            password.setBackgroundResource(R.drawable.edittext_error_background);
+            Toast.makeText(RegisterActivity.this, R.string.error_registration_password, Toast.LENGTH_SHORT).show();
+            isValid = false;
+        }
+
+        return isValid;
     }
 
     @Override
@@ -167,21 +204,19 @@ public class RegisterActivity extends BaseActivity
 
     private void initRegisterListener()
     {
-        email = (EditText) findViewById(R.id.email);
-        password = (EditText) findViewById(R.id.password);
-        firstName = (EditText) findViewById(R.id.first_name);
-
         findViewById(R.id.register).setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View view)
             {
-                String emailText = email.getText().toString();
-                String passtext = password.getText().toString();
-                String firsttext = firstName.getText().toString();
+                if (validateForRegistration()) {
+                    String emailText = email.getText().toString();
+                    String passtext = password.getText().toString();
+                    String firsttext = firstName.getText().toString();
 
-                TaskQueue.loadQueueDefault(RegisterActivity.this).execute(
-                        new RegisterTask(emailText, passtext, firsttext));
+                    TaskQueue.loadQueueDefault(RegisterActivity.this).execute(
+                            new RegisterTask(emailText, passtext, firsttext));
+                }
             }
         });
     }
