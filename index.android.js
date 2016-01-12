4f15b4d5b9b43d0d19d9dd014431ba13744e93fe
Jon Uy <jonxuy@gmail.com>
2016-01-12 17:00:06 -0500

    A working version of the react-native tutorial - based off of this here thing: https://facebook.github.io/react-native/docs/tutorial.html

--- index.android.js
+++ index.android.js
@@ -2,28 +2,110 @@
 
 var React = require('react-native');
 var {
+  Image,
+  ListView,
   Text,
   View
 } = React;
 
-class ReactPrototype extends React.Component {
-  render() {
+var MOCKED_MOVIES_DATA = [
+  {title: 'Title', year: '2016', posters: {thumbnail: 'http://i.imgur.com/RvDSh4Z.jpg'}}
+];
+var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
+
+var ReactPrototype = React.createClass({
+  render: function() {
+    if (!this.state.loaded) {
+      return this.renderLoadingView();
+    }
+
+    return (
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={this.renderMovie}
+        style={styles.listView}
+        />
+    );
+//    var movie = this.state.movies[0];
+//    return this.renderMovie(movie);
+  },
+
+  renderLoadingView: function() {
     return (
       <View style={styles.container}>
-        <Text style={styles.hello}>Hello, World</Text>
+        <Text>
+          Loading movies...
+        </Text>
       </View>
-    )
-  }
-}
+    );
+  },
+
+  renderMovie: function(movie) {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={{uri: movie.posters.thumbnail}}
+          style={styles.thumbnail} />
+        <View style={styles.rightContainer}>
+          <Text style={styles.title}>{movie.title}</Text>
+          <Text style={styles.year}>{movie.year}</Text>
+        </View>
+      </View>
+    );
+  },
+
+  componentDidMount: function() {
+    this.fetchData();
+  },
+
+  fetchData: function() {
+    fetch(REQUEST_URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.setState({
+          dataSource: this.state.dataSource.cloneWithRows(responseData.movies),
+          loaded: true,
+        });
+      })
+      .done();
+  },
+
+  getInitialState: function() {
+    return {
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      }),
+      loaded: false,
+    };
+  },
+});
+
 var styles = React.StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
   },
-  hello: {
+  rightContainer: {
+    flex: 1
+  },
+  listView: {
+    paddingTop: 20,
+    backgroundColor: '#F5FCFF',
+  },
+  thumbnail: {
+    width: 53,
+    height: 81,
+  },
+  title: {
     fontSize: 20,
+    marginBottom: 8,
     textAlign: 'center',
-    margin: 10,
+  },
+  year: {
+    textAlign: 'center'
   },
 });
 
