5910d688c399704ccc5fa38c31a3febd2a41b23e
Jon Uy <jonxuy@gmail.com>
2016-02-18 17:53:07 -0500

    Removing unused Hub code

--- app/src/main/java/org/dosomething/letsdothis/ui/adapters/HubAdapter.java
+++ app/src/main/java/org/dosomething/letsdothis/ui/adapters/HubAdapter.java
@@ -1,9 +1,5 @@
 package org.dosomething.letsdothis.ui.adapters;
 import android.content.Context;
-import android.content.Intent;
-import android.content.res.Resources;
-import android.graphics.ColorMatrix;
-import android.graphics.ColorMatrixColorFilter;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,11 +14,8 @@ import org.dosomething.letsdothis.R;
 import org.dosomething.letsdothis.data.Campaign;
 import org.dosomething.letsdothis.data.ReportBack;
 import org.dosomething.letsdothis.data.User;
-import org.dosomething.letsdothis.data.UserReportBack;
 import org.dosomething.letsdothis.network.models.ResponseProfileCampaign;
 import org.dosomething.letsdothis.network.models.ResponseProfileSignups;
-import org.dosomething.letsdothis.ui.CampaignDetailsActivity;
-import org.dosomething.letsdothis.ui.ReportBackDetailsActivity;
 
 import java.util.ArrayList;
 import java.util.Locale;
@@ -34,12 +27,10 @@ public class HubAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     //~=~=~=~=~=~=~=~=~=~=~=~=Constants
     private static final int    VIEW_TYPE_PROFILE          = 0;
     private static final int    VIEW_TYPE_SECTION_TITLE    = 1;
-    private static final int    VIEW_TYPE_CURRENT_CAMPAIGN = 2;
-    private static final int    VIEW_TYPE_PAST_CAMPAIGN    = 3;
-    private static final int    VIEW_TYPE_CURRENT_EMPTY    = 4;
-    private static final int    VIEW_TYPE_PUBLIC_EMPTY     = 5;
-    private static final int    VIEW_TYPE_CURRENT_SIGNUPS  = 6;
-    private static final int    VIEW_TYPE_REPORTBACKS      = 7;
+    private static final int    VIEW_TYPE_CURRENT_EMPTY    = 2;
+    private static final int    VIEW_TYPE_PUBLIC_EMPTY     = 3;
+    private static final int    VIEW_TYPE_CURRENT_SIGNUPS  = 4;
+    private static final int    VIEW_TYPE_REPORTBACKS      = 5;
 
     private static final String CURRENT_SIGNUPS_LABEL_STUB = "PLACEHOLDER: CURRENT_SIGNUPS_LABEL_STUB";
     private static final String CURRENT_CAMPAIGNS_EMPTY_STUB = "PLACEHOLDER: CURRENT_CAMPAIGNS_EMPTY_STUB";
@@ -50,13 +41,11 @@ public class HubAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     private HubAdapterClickListener mHubAdapterClickListener;
     private boolean mIsPublic = false;
     private Campaign mClickedCampaign;
-    private Context mContext;
     private User mUser;
 
     public HubAdapter(Context context, HubAdapterClickListener hubAdapterClickListener, boolean isPublic) {
         super();
 
-        this.mContext = context;
         this.mHubAdapterClickListener = hubAdapterClickListener;
 
         // First row is user profile info
@@ -80,15 +69,6 @@ public class HubAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
                 v = LayoutInflater.from(parent.getContext())
                         .inflate(R.layout.item_hub_profile, parent, false);
                 return new ProfileViewHolder(v);
-            case VIEW_TYPE_CURRENT_CAMPAIGN:
-                v = LayoutInflater.from(parent.getContext())
-                        .inflate(R.layout.item_hub_current_campaign, parent, false);
-                return new CurrentCampaignViewHolder(v);
-            // @todo May not make sense anymore. Consider replacing it with something like a VIEW_TYPE_REPORTBACKS
-            case VIEW_TYPE_PAST_CAMPAIGN:
-                v = LayoutInflater.from(parent.getContext())
-                        .inflate(R.layout.item_hub_past_campaign, parent, false);
-                return new PastCampaignViewHolder(v);
             case VIEW_TYPE_CURRENT_EMPTY:
                 v = LayoutInflater.from(parent.getContext())
                             .inflate(R.layout.item_hub_current_empty, parent, false);
@@ -143,117 +123,6 @@ public class HubAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
             profileViewHolder.name.setText(formatUserDisplayName(user.first_name, user.last_initial));
             profileViewHolder.userCountry.setText(formatUserLocation(user.country));
             profileViewHolder.userCountry.setAlpha(0.26f);
-        }
-        else if (getItemViewType(position) == VIEW_TYPE_CURRENT_CAMPAIGN) {
-            final Campaign campaign = (Campaign) mHubList.get(position);
-            CurrentCampaignViewHolder viewHolder = (CurrentCampaignViewHolder) holder;
-            viewHolder.title.setText(campaign.title);
-            viewHolder.taglineCaption.setText(campaign.callToAction);
-
-            Resources res = viewHolder.title.getResources();
-
-            if (!mIsPublic && campaign.showShare == Campaign.UploadShare.SHARE) {
-                viewHolder.share.setText(res.getString(R.string.share_photo));
-
-                // Clicking on the Share button should share the reportback
-                viewHolder.share.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        if (campaign.showShare == Campaign.UploadShare.SHARE) {
-                            mHubAdapterClickListener.onShareClicked(campaign);
-                            mClickedCampaign = campaign;
-                        }
-                    }
-                });
-            }
-            else {
-                viewHolder.share.setVisibility(View.GONE);
-            }
-
-            boolean hasReportBackImage = false;
-            String tmpRbItemId = "";
-            if (campaign.userReportBack != null) {
-                String count = String.format("%d %s %s", campaign.userReportBack.quantity,
-                        campaign.noun, campaign.verb);
-                viewHolder.count.setText(count);
-
-                ArrayList<UserReportBack.ReportBackItem> items = campaign.userReportBack.getItems();
-                if (items != null && items.size() > 0) {
-                    hasReportBackImage = true;
-
-                    // We'll just default to showing the first image in the reportback list
-                    String reportBackImage = items.get(0).getImagePath();
-                    tmpRbItemId = items.get(0).getId();
-
-                    int height = mContext.getResources().getDimensionPixelSize(R.dimen.campaign_height);
-                    Picasso.with(mContext).load(reportBackImage).resize(0, height)
-                            .into(viewHolder.reportbackImage);
-
-                    // And use this image's caption
-                    viewHolder.taglineCaption.setText(items.get(0).getCaption());
-                }
-            }
-
-            if (hasReportBackImage) {
-                final String rbItemId = tmpRbItemId;
-                viewHolder.imageContainer.setVisibility(View.VISIBLE);
-                viewHolder.addImage.setVisibility(View.GONE);
-                viewHolder.reportbackImage.setVisibility(View.VISIBLE);
-
-                // Clicking the reportback image should go to that reportback's detail screen
-                viewHolder.reportbackImage.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent intent = ReportBackDetailsActivity.getLaunchIntent(mContext,
-                                Integer.parseInt(rbItemId), campaign.id);
-                        mContext.startActivity(intent);
-                    }
-                });
-            }
-            else {
-                // Use campaign's call-to-action if no reportback
-                viewHolder.taglineCaption.setText(campaign.callToAction);
-
-                if (!mIsPublic) {
-                    viewHolder.imageContainer.setVisibility(View.VISIBLE);
-                    viewHolder.addImage.setVisibility(View.VISIBLE);
-                    viewHolder.reportbackImage.setVisibility(View.GONE);
-
-                    // Clicking the "add image" button should start the reportback flow
-                    viewHolder.addImage.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View view) {
-                            mHubAdapterClickListener.onProveClicked(campaign);
-                            mClickedCampaign = campaign;
-                        }
-                    });
-                }
-                else {
-                    // Hide the image container if it's a public profile with no reportback
-                    viewHolder.imageContainer.setVisibility(View.GONE);
-                }
-            }
-
-            // Clicking on campaign title should go to the Campaign Details screen
-            viewHolder.title.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    mContext.startActivity(CampaignDetailsActivity.getLaunchIntent(mContext, campaign.id));
-                }
-            });
-        }
-        else if(getItemViewType(position) == VIEW_TYPE_PAST_CAMPAIGN) {
-            Campaign campaign = (Campaign) mHubList.get(position);
-            PastCampaignViewHolder pastCampaignViewHolder = (PastCampaignViewHolder) holder;
-            Context context = pastCampaignViewHolder.image.getContext();
-            int height = context.getResources().getDimensionPixelSize(R.dimen.campaign_height);
-            Picasso.with(context).load(campaign.imagePath).resize(0, height)
-                    .into(pastCampaignViewHolder.image);
-            ColorMatrix cm = new ColorMatrix();
-            cm.setSaturation(0);
-            pastCampaignViewHolder.image.setColorFilter(new ColorMatrixColorFilter(cm));
-
-            pastCampaignViewHolder.title.setText(campaign.title);
         }
         else if (getItemViewType(position) == VIEW_TYPE_CURRENT_SIGNUPS) {
             ResponseProfileCampaign campaign = (ResponseProfileCampaign) mHubList.get(position);
@@ -326,9 +195,6 @@ public class HubAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
         }
         else if (currentObject instanceof User) {
             return VIEW_TYPE_PROFILE;
-        }
-        else if (currentObject instanceof Campaign) {
-            return VIEW_TYPE_CURRENT_CAMPAIGN;
         }
         else if (currentObject instanceof ResponseProfileCampaign) {
             return VIEW_TYPE_CURRENT_SIGNUPS;
